c4711bf5463225d0148e66b435691f99a60b7806
Ajay Kumar Shukla <61351885+AjayShukla1212@users.noreply.github.com>
2024-06-29 16:48:46 +0530

    Added assigning different properties for running application at 1.2.3.4:1234

---
+++ agent/src/main/java/com/ajay/mocklibimpl/agent/constants/MockConstants.java
@@ -0,0 +1,11 @@
+package com.ajay.mocklibimpl.agent.constants;
+
+public class MockConstants {
+    public static final String REST_TEMPLATE = "RestTemplate";
+    public static final String MOCK_DATA = "{\"datetime\":\"2024-06-1-T12:34:56.789Z\"}";
+    public static final String GET_OBJECT = "getForObject";
+    public static final String HT_MODE = "HT_MODE";
+    public static final String REPLAY_MODE = "REPLAY";
+    public static final String POST_REPO = "PostRepository";
+    public static final String SAVE = "save";
+}
