8db556652d452e11ce6aee0de150fe8f61f5f4b2
Ajay Kumar Shukla <61351885+AjayShukla1212@users.noreply.github.com>
2024-07-09 18:47:27 +0530

    Pushed changes for Mocking JPA

---
+++ agent/src/main/java/com/ajay/mocklibimpl/agent/MockDBAdvice.java
@@ -0,0 +1,50 @@
+package com.ajay.mocklibimpl.agent;
+
+import net.bytebuddy.asm.Advice;
+import net.bytebuddy.implementation.bytecode.assign.Assigner;
+
+import java.lang.reflect.Field;
+
+public class MockDBAdvice {
+
+    @Advice.OnMethodEnter(skipOn = Advice.OnNonDefaultValue.class)
+    public static Object interceptSave(@Advice.Argument(0) Object entity) throws IllegalAccessException, InstantiationException {
+        System.out.println("Intercepted save method with entity: " + entity);
+        return mockResult(entity);
+    }
+
+    //currently not in use
+    @Advice.OnMethodExit(onThrowable = Throwable.class)
+    public static void onExit(@Advice.Enter Object mockEntity,
+                              @Advice.Return(readOnly = false, typing = Assigner.Typing.DYNAMIC) Object result,
+                              @Advice.Thrown Throwable throwable) {
+        if (throwable != null) {
+            System.out.println("DB Exception::" + throwable.getMessage());
+        } else {
+            result = mockEntity;
+            System.out.println("Returning mocked entity: " + mockEntity);
+        }
+    }
+    public static Object mockResult(Object result) throws IllegalAccessException, InstantiationException {
+        System.out.println("Mocking result object: " + result.toString());
+        //create mocked response object
+        Object mockEntity = result.getClass().newInstance();
+        for (Field field : mockEntity.getClass().getDeclaredFields()) {
+            field.setAccessible(true);
+            //setting id
+            if (field.getName().equalsIgnoreCase("id") && field.getType() == Long.class) {
+                field.set(mockEntity, 1L);
+            } else if (field.getType() == String.class) {
+                //setting name
+                if (field.getName().equalsIgnoreCase("name")) {
+                    field.set(mockEntity, "Test Post 1");
+                } else if (field.getName().equalsIgnoreCase("contents")) { //setting contents
+                    field.set(mockEntity, "This is a test post content.");
+                } else {
+                    field.set(mockEntity, "Mocked String"); //setting extra parameters if any
+                }
+            }
+        }
+        return mockEntity;
+    }
+}
